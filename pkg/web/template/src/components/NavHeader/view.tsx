5dad330a39695a3ccf7830bb320ac29e8363764d
xcphoenix <root@xcphoenix.top>
2021-03-09 21:11:47 +0800

    fix(fronted): reponsive design, show table use scroll

--- pkg/web/template/src/components/NavHeader/view.tsx
+++ pkg/web/template/src/components/NavHeader/view.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import { Navbar, Nav, Icon } from 'rsuite';
+import { Navbar, Nav, Icon, NavItemProps } from 'rsuite';
 import 'rsuite/lib/Nav/styles';
 import 'rsuite/lib/Navbar/styles';
 import 'rsuite/lib/Icon/styles';
 import './index.css';
+
+const MyLink = React.forwardRef((props, ref) => {
+  // @ts-ignore
+  const { href, ...rest } = props;
+  // @ts-ignore
+  return (
+    <a href={href} {...rest} />
+  );
+});
+
+const NavLink = (props: JSX.IntrinsicAttributes & NavItemProps) =>
+  <Nav.Item componentClass={MyLink} {...props} />;
 
 const NavHeader = () => {
   return (
@@ -15,8 +27,10 @@ const NavHeader = () => {
         </div>
       </Navbar.Header>
       <Navbar.Body>
-        <Nav pullRight href={'https://github.com/PhoenixXC/elf-load-analyser'}>
-          <Nav.Item icon={<Icon icon='github' />}>Github</Nav.Item>
+        <Nav pullRight={true}>
+          <NavLink href={'https://github.com/PhoenixXC/elf-load-analyser'}
+                   target={'_blank'}
+                   icon={<Icon icon='github' />}>Github</NavLink>
         </Nav>
       </Navbar.Body>
     </Navbar>
