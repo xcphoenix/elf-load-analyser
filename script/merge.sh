b089034569f746eb8eeb5fc66496c7cc08289e6a
xcphoenix <root@xcphoenix.top>
2021-03-10 23:11:46 +0800

    build(script): pre_process.sh use yarn build cache

--- script/merge.sh
+++ script/merge.sh
@@ -3,6 +3,7 @@
 source ./util.sh
 
 readonly OUT_SUFFIX="k"
+readonly FRONTED_CHECKSUM="md5sums"
 
 function merge_src() {
     rm_file "*.${OUT_SUFFIX}"
@@ -18,8 +19,30 @@ function merge_src() {
     done
 }
 
+function get_md5sums() {
+    find ./ -type f \
+            -not -path "./node_modules/*"\
+            -not -path ".*/.umi/*"\
+            -not -path ".*/.idea/*" \
+            -not -path "./${FRONTED_CHECKSUM}"\
+            -exec md5sum {} \; | sort -k 2 | md5sum | awk '{print $1}'
+}
+
 function build_fronted() {
+    if (( $# > 0 )) && [ -f "${FRONTED_CHECKSUM}" ] && [ -d "dist" ]; then
+        old_checksum=$(cat "${FRONTED_CHECKSUM}")
+        new_checksum=$(get_md5sums)
+        echo "old_checksum: ${old_checksum}"
+        echo "new_checksum: ${new_checksum}"
+        if [ "${old_checksum}" == "${new_checksum}" ]; then
+            echo "file not change"
+            return
+        fi
+    fi
+
+    echo "build fronted..."
     export MOCK=none
     yarn
     yarn build
+    get_md5sums > "${FRONTED_CHECKSUM}"
 }
--- script/pre_process.sh
+++ script/pre_process.sh
@@ -7,4 +7,4 @@ readonly THIS_DIR="$(pwd)"
 cd "${THIS_DIR}/../pkg/modules/module/src" || exit
 merge_src
 cd "${THIS_DIR}/../pkg/web/template" || exit
-build_fronted
+build_fronted 'use cache'
